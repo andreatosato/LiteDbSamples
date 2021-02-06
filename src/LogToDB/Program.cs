e0a791543ab750f16587893b5c24d16118c7cc6e
Andrea Tosato <andrea.tosato@hotmail.it>
2021-02-06 11:35:44 +0100

    Start

---
+++ src/LogToDB/Program.cs
@@ -0,0 +1,19 @@
+﻿using Serilog;
+using System;
+
+namespace LogToDB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            
+            var log = new LoggerConfiguration()
+                .WriteTo.LiteDB(@$"{AppContext.BaseDirectory}\logs\applog.db", logCollectionName: "applog")
+                .CreateLogger();
+
+            log.Error("Errore");
+        }
+    }
+}
